5ba1076da28218a0a784a6f1ce35a9238ba06b2a
Ricardo Martín Rodriguez García <ricardomartin.rodriguez@fisglobal.com>
2019-11-20 18:26:36 -0500

    First commit

---
+++ mini-swift-app/mini-swift-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mini-swift-app
+//
+//  Created by Ricardo Martín Rodriguez García on 11/20/19.
+//  Copyright © 2019 RixStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mini-swift-app/mini-swift-appTests/mini_swift_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  mini_swift_appTests.swift
+//  mini-swift-appTests
+//
+//  Created by Ricardo Martín Rodriguez García on 11/20/19.
+//  Copyright © 2019 RixStudio. All rights reserved.
+//
+
+import XCTest
+@testable import mini_swift_app
+
+class mini_swift_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
